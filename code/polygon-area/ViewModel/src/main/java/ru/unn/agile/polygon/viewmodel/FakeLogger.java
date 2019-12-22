707c594ca9fd4e8ca96dbf9bf101fd44f081c9ae
AndrewSmirnov <smirnov-andrew7@mail.ru>
2019-12-22 19:01:24 +0300

    Introducing Infrastructure module. Adding ILogger interface and fake logger implementation

---
+++ code/polygon-area/ViewModel/src/main/java/ru/unn/agile/polygon/viewmodel/FakeLogger.java
@@ -0,0 +1,20 @@
+package ru.unn.agile.polygon.viewmodel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeLogger implements ILogger {
+
+    private final ArrayList<String> log = new ArrayList<>();
+
+    @Override
+    public void log(final String s) {
+        log.add(s);
+    }
+
+    @Override
+    public List<String> getLog() {
+        return log;
+    }
+
+}
---
+++ code/polygon-area/ViewModel/src/main/java/ru/unn/agile/polygon/viewmodel/ILogger.java
@@ -0,0 +1,11 @@
+package ru.unn.agile.polygon.viewmodel;
+
+import java.util.List;
+
+public interface ILogger {
+
+    void log(String s);
+
+    List<String> getLog();
+
+}
