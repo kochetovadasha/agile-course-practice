c1421c6e6591ec67fa1a136e06d1f92cb4338aa3
Kirill <Petrovich_1811@mail.ru>
2019-12-22 20:37:10 +0300

    А я уж думал, что больше бесполезным вещами не придется заниматься

--- code/polynomial-calculator/ViewModel/src/main/java/ru/unn/agile/polynomialcalculator/viewmodel/ViewModel.java
+++ code/polynomial-calculator/ViewModel/src/main/java/ru/unn/agile/polynomialcalculator/viewmodel/ViewModel.java
@@ -9,12 +9,8 @@ import ru.unn.agile.polynomialcalculator.model.Polynomial;
 import java.util.regex.Pattern;
 
 public class ViewModel {
-    private Polynomial polynomial;
-
     private final StringProperty degree1 = new SimpleStringProperty();
-    private final StringProperty degree2 = new SimpleStringProperty();
     private final StringProperty coeff1 = new SimpleStringProperty();
-    private final StringProperty coeff2 = new SimpleStringProperty();
 
     private static final Pattern COEFF_INPUT_ALLOWED_SYMBOLS =
             Pattern.compile("^[-+]?[0-9]+\\.?[0-9]*$");
@@ -22,23 +18,9 @@ public class ViewModel {
     private static final Pattern DEGREE_INPUT_ALLOWED_SYMBOLS =
             Pattern.compile("^[0-9]+$");
 
-    private final ObservableList<Polynomial> pointList = FXCollections.observableArrayList();
+    private final ObservableList<Polynomial> polynomialsList = FXCollections.observableArrayList();
 
     private final StringProperty result = new SimpleStringProperty();
-
-    public ViewModel() {
-        clearFormInput();
-
-        BooleanBinding canCalculateBoolBinding = new BooleanBinding() {
-            {
-                super.bind(degree1, degree2, coeff1, coeff2);
-            }
-            @Override
-            protected boolean computeValue() {
-                return (isCoordinatesInputCorrect());
-            }
-        };
-    }
 
     public boolean isPolynomialInputCorrect() {
         String degree = degree1.get();
@@ -48,59 +30,61 @@ public class ViewModel {
                 && DEGREE_INPUT_ALLOWED_SYMBOLS.matcher(degree).matches());
     }
 
+    public ViewModel() {
+        clearFormInput();
+
+        BooleanBinding canCalculateBoolBinding = new BooleanBinding() {
+            {
+                super.bind(degree1, coeff1);
+            }
+            @Override
+            protected boolean computeValue() {
+                return (isPolynomialInputCorrect());
+            }
+        };
+    }
+
     public void addPolynomial() {
-        int degree = parseDegree(degree1);
-        double coeff = parseCoeff(coeff1);
-        pointList.add(new Polynomial(coeff, degree));
+        var polynomial = parsePolynomial(coeff1, degree1);
+        polynomialsList.add(polynomial);
         clearFormInput();
     }
 
-    public void calcPolynomial() {
-        if (pointList.isEmpty()) {
+    public void calcPolynomialAdd() {
+        if (polynomialsList.isEmpty()) {
             return;
         }
-
         try {
-            Polynomial p1 = new Polynomial(pol.get(), degree1.get());
-            Polynomial p2 = new Polynomial(re2.get(), degree2.get());
-
-            result.set(operation.get().apply(z1, z2).toString());
-            status.set(Status.SUCCESS.toString());
-
-            polynomial = new Polynomial(pointArray);
-//            result.setValue(Double.toString(polynomial.getArea()));
+            Polynomial polySum = new Polynomial(0, 0);
+            for (Polynomial poly : polynomialsList)  {
+                polySum.plus(poly);
+            }
+            result.set(polySum.toString());
         } catch (IllegalArgumentException e) {
             result.setValue(e.getMessage());
         }
     }
 
     private void clearFormInput() {
-        xCoordinate.set("");
-        yCoordinate.set("");
+        degree1.set("");
+        coeff1.set("");
     }
 
-    private int parseDegree(final StringProperty degree) {
-        if (!isCoordinatesInputCorrect()) {
-            throw new IllegalArgumentException("Can't parse degree. Invalid input");
+    private Polynomial parsePolynomial(final StringProperty coeff, final StringProperty degree) {
+        if (!isPolynomialInputCorrect()) {
+            throw new IllegalArgumentException("Can't parse polynomial. Invalid input");
         }
-        return Integer.parseInt(degree.get());
+        return new Polynomial(Double.parseDouble(coeff.get()), Integer.parseInt(degree.get()));
     }
 
-    private double parseCoeff(final StringProperty coeff) {
-        if (!isCoordinatesInputCorrect()) {
-            throw new IllegalArgumentException("Can't parse coeff. Invalid input");
-        }
-        return Double.parseDouble(coeff.get());
+    public StringProperty degreeProperty() {
+        return degree1;
     }
-
-    public StringProperty xProperty() {
-        return xCoordinate;
+    public StringProperty coeffProperty() {
+        return coeff1;
     }
-    public StringProperty yProperty() {
-        return yCoordinate;
-    }
-    public ObservableList<Point> getPointList() {
-        return pointList;
+    public ObservableList<Polynomial> getpolynomialsList() {
+        return polynomialsList;
     }
 
     public StringProperty resultProperty() {
@@ -110,10 +94,4 @@ public class ViewModel {
         return result.get();
     }
 
-    public BooleanProperty addingNewPointDisabledProperty() {
-        return addingNewPointDisabled;
-    }
-    public final boolean isAddingNewPointDisabled() {
-        return addingNewPointDisabled.get();
-    }
 }
