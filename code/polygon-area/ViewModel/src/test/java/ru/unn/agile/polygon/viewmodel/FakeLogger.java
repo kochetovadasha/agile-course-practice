921ab1cb263e5923ae589653fdbdd9f64a1b7900
AndrewSmirnov <smirnov-andrew7@mail.ru>
2019-12-24 21:37:57 +0300

    FakeLogger fix

--- code/polygon-area/ViewModel/src/test/java/ru/unn/agile/polygon/viewmodel/FakeLogger.java
+++ code/polygon-area/ViewModel/src/test/java/ru/unn/agile/polygon/viewmodel/FakeLogger.java
@@ -8,8 +8,8 @@ public class FakeLogger implements ILogger {
     private final ArrayList<String> log = new ArrayList<>();
 
     @Override
-    public void log(final String s) {
-        log.add(s);
+    public void log(final String logMessage) {
+        log.add(logMessage);
     }
 
     @Override
