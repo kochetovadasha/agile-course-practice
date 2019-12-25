676c0d1168b80b34a291785e7eccefff549d45b9
Kalyanov Dmitry <kalyanowdima@gmail.ru>
2019-12-25 16:19:35 +0300

    fix travis

--- code/dijkstra-algorithm/ViewModel/src/test/java/ru/unn/agile/dijkstraalgorithm/viewmodel/EmptyLogger.java
+++ code/dijkstra-algorithm/ViewModel/src/test/java/ru/unn/agile/dijkstraalgorithm/viewmodel/EmptyLogger.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EmptyLogger implements ILogger {
-    private final ArrayList<String> log = new ArrayList<>();
+    @Override
+    public void log(final String s) {
+        log.add(s);
+    }
 
     @Override
     public List<String> getLog() {
         return log;
     }
 
-    @Override
-    public void log(final String s) {
-        log.add(s);
-    }
+    private final ArrayList<String> log = new ArrayList<>();
 }
